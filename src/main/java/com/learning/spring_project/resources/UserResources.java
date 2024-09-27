cc63934af23ad89859bca507d94097a9aee27a3c
Marcos Oliveira <mspol23@gmail.com>
2024-09-27 12:44:45 -0300

    User entity and resource

---
+++ src/main/java/com/learning/spring_project/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.learning.spring_project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.spring_project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria Soares", "maria@gmail.com", "999992222", "qwerty123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
