5eb6de8c0f5d9bd3ae6f318ddb7d1abc6a29a80e
Marcos Oliveira <mspol23@gmail.com>
2024-10-01 08:57:01 -0300

    Exception handler for findById method.

--- src/main/java/com/learning/spring_project/services/UserService.java
+++ src/main/java/com/learning/spring_project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.learning.spring_project.entities.User;
 import com.learning.spring_project.repositories.UserRepository;
+import com.learning.spring_project.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User newUser) {
